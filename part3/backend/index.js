8eacc84bd57cd5e891f558b9042bf268e0b0afad
Aarón Jamet Orgilés <aaronjametorgiles@gmail.com>
2024-06-29 00:54:37 +0200

    5

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -107,5 +107,5 @@ app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3008
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 })
