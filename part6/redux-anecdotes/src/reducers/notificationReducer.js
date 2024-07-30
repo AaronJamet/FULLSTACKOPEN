b66a5c84e5725d8a57e20a852539a2716770f508
Aarón Jamet Orgilés <aaronjametorgiles@gmail.com>
2024-07-30 10:19:11 +0200

    erasing old code from notificationReducer

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -2,24 +2,6 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = null
 
-export const handleVoteNotification = (anecdote) => (dispatch) => {
-  dispatch(clearNotification())
-  dispatch(voteNotification(anecdote))
-  setTimeout(() => {
-    console.log('Clearing notification')
-    dispatch(clearNotification())
-  }, 5000)
-}
-
-export const handleCreateNotification = (anecdote) => (dispatch) => {
-  dispatch(clearNotification());
-  dispatch(createNotification(anecdote));
-  setTimeout(() => {
-    console.log('Clearing notification');
-    dispatch(clearNotification());
-  }, 5000)
-}
-
 const notificationSlice = createSlice({
   name: 'notification',
   initialState,
