b8fd936dfe491bf3ab97837a8a80701de4e5b1c2
Aarón Jamet Orgilés <aaronjametorgiles@gmail.com>
2024-08-22 15:21:51 +0200

    Part 8B - exercises from 8.8 to 8.12

---
+++ part8/library-frontend/src/components/Authors.jsx
@@ -0,0 +1,104 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+import Select from 'react-select'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Authors = (props) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const [ changeAuthor ] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map(e => e.message).join('\n')
+      console.log(messages)
+    }
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    changeAuthor({ variables: { name, setBornTo: parseInt(born) }})
+
+    setName('')
+    setBorn('')
+  }
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading) {
+    return <p>Loading...</p>
+  }
+
+  if (result.error) {
+    <p>Error: {result.error.message}</p>
+  }
+
+  const authors = result.data.allAuthors
+
+  const options =  authors.map((a) => ({
+                    value: a.name, 
+                    label: a.name
+                  }))
+  
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h2>Set birhtyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          <Select 
+            defaultValue={name}
+            onChange={(selectedAuthor) => setName(selectedAuthor.value)}
+            options={options}
+            styles={{ 
+              control: (provided) => ({
+                ...provided,
+                width: '300px',
+              })
+            }}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+Authors.propTypes = {
+  show: PropTypes.bool.isRequired
+}
+
+export default Authors
---
+++ part8/library-frontend/src/components/NewBook.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import PropTypes from 'prop-types'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
+
+const NewBook = (props) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ],
+    onError: (error) => {
+      const messages = error.graphQLErrors.map(e => e.message).join('\n')
+      console.log(messages)
+    }
+  })
+
+  if (!props.show) {
+    return null
+  }
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    createBook({ variables: { title, author, 
+      published: parseInt(published), genres } })
+
+    setTitle('')
+    setAuthor('')
+    setPublished('')
+    setGenres([])
+    setGenre('')
+  }
+
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          title
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          published
+          <input
+            type="number"
+            value={published}
+            onChange={({ target }) => setPublished(target.value)}
+          />
+        </div>
+        <div>
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+          <button onClick={addGenre} type="button">
+            add genre
+          </button>
+        </div>
+        <div>genres: {genres.join(' ')}</div>
+        <button type="submit">create book</button>
+      </form>
+    </div>
+  )
+}
+
+NewBook.propTypes = {
+  show: PropTypes.bool.isRequired
+}
+
+export default NewBook
